86b38cd4b5ca8fb7279e2784b1c27a62c3f6c798
Dean <o.michaeldean@gmail.com>
2024-10-05 13:13:21 +0100

    deployed smart contracts

--- eventos_contract/ignition/modules/Lock.ts
+++ eventos_contract/ignition/modules/Lock.ts
@@ -9,14 +9,14 @@ const LockModule = buildModule("LockModule", (m) => {
   );
   const _paymentToken = m.getParameter(
     "_paymentToken",
-    "0x442576ef8EA93B6aA30cb7C779b8cC1e402bca5e"
+    "0x94b38F87880912aa4F46b4A7631315B933f67A2c"
   );
   const nftAddress = m.getParameter(
     "nftAddress",
-    "0xBbcD22fd30EFA3c859f3C30a7224aB257D20b112"
+    "0x442576ef8EA93B6aA30cb7C779b8cC1e402bca5e"
   );
 
-  const lock = m.contract("Ventura", [owner, _paymentToken, nftAddress ]);
+  const lock = m.contract("Ventura", [owner, _paymentToken, nftAddress]);
 
   return { lock };
 });
@@ -24,6 +24,6 @@ const LockModule = buildModule("LockModule", (m) => {
 export default LockModule;
 
 //Owner: 0xF2E7E2f51D7C9eEa9B0313C2eCa12f8e43bd1855
-//StableToken: 0x442576ef8EA93B6aA30cb7C779b8cC1e402bca5e
-//VenturaTokens: 0xBbcD22fd30EFA3c859f3C30a7224aB257D20b112
-//Ventura: 0x97D9bcE273974455Bfc3A51E8Fd956D4209066A3
+//StableToken: 0x94b38F87880912aa4F46b4A7631315B933f67A2c
+//VenturaTokens: 0x442576ef8EA93B6aA30cb7C779b8cC1e402bca5e
+//Ventura: 0xBbcD22fd30EFA3c859f3C30a7224aB257D20b112
